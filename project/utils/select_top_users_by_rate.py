e57169c4a48056aea76118eef4277a4044037168
Dmitrii Savichev <kubamba2002@gmail.com>
2023-03-08 00:12:43 +0300

    homework-01

---
+++ project/utils/select_top_users_by_rate.py
@@ -0,0 +1,5 @@
+from models.user import User
+
+
+def select_top_users_by_rate(users: list[User], top_size: int) -> list[User]:
+    return sorted(users, key=lambda x: x.rate, reverse=True)[:top_size]
